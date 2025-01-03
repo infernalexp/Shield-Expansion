429f1a9b4838edf1250a59a8c880a715641adad2
tinytransfem <tinytransfem@gmail.com>
2025-01-03 01:31:12 +0100

    fix blocking state persisting after attack (for real this time)

--- src/main/java/org/infernalstudios/shieldexp/init/NetworkInit.java
+++ src/main/java/org/infernalstudios/shieldexp/init/NetworkInit.java
@@ -5,6 +5,7 @@ import net.minecraftforge.network.NetworkRegistry;
 import net.minecraftforge.network.simple.SimpleChannel;
 import org.infernalstudios.shieldexp.ShieldExpansion;
 import org.infernalstudios.shieldexp.network.ClearShields;
+import org.infernalstudios.shieldexp.network.SyncBlocking;
 import org.infernalstudios.shieldexp.network.SyncShields;
 
 public class NetworkInit {
@@ -19,6 +20,7 @@ public class NetworkInit {
         INSTANCE = NetworkRegistry.newSimpleChannel(new ResourceLocation(ShieldExpansion.MOD_ID, "packets"), () -> "1.0", s -> true, s -> true);
 
         INSTANCE.registerMessage(nextID(), SyncShields.class, SyncShields::encode, SyncShields::new, SyncShields::handle);
+        INSTANCE.registerMessage(nextID(), SyncBlocking.class, SyncBlocking::encode, SyncBlocking::new, SyncBlocking::handle);
 
         // TODO Delete - This is only kept here so the network isn't angry
         INSTANCE.registerMessage(nextID(), ClearShields.class, ClearShields::encode, ClearShields::new, ClearShields::handle);
